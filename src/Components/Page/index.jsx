84e085a4cf1e55b7bb7624fa124ce9cd1f18aac5
Nalinee-ss <nalinee.rajan@softsuave.com>
2023-07-07 20:26:04 +0530

    solved the routing sissue

--- src/Components/Page/index.jsx
+++ src/Components/Page/index.jsx
@@ -3,8 +3,18 @@ import { Box } from "@mui/material";
 import Information from "./Information";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 const Page = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location?.pathname === "/") {
+      navigate("/personalinfo");
+    }
+  }, []);
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Box sx={{ width: 1, height: 1 }}>
